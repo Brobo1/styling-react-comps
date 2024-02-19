ef19233dbca4abdc904061bec58a26cd46ac68a5
brobo1 <armanahz95@gmail.com>
2024-02-19 11:38:31 +0100

    Update Header and AuthInputs with styled-components
    Refactored Header and AuthInputs components to use styled-components for styling. Previously used CSS modules in Header were replaced with a StyledHeader component styled inline using styled-components. Also added a new StyledButton component to AuthInputs, providing cleaner code and easier style manipulation.

--- src/components/AuthInputs.jsx
+++ src/components/AuthInputs.jsx
@@ -17,6 +17,7 @@ const Label = styled.label`
     text-transform: uppercase;
     color: ${({$invalid}) => $invalid ? "#f87171" : "#6b7280"};
 `
+
 const Input = styled.input`
     width: 100%;
     padding: 0.75rem 1rem;
@@ -26,6 +27,21 @@ const Input = styled.input`
     border: 1px solid ${({$invalid}) => $invalid ? "#f73f3f" : "transparent"};
     border-radius: 0.25rem;
     box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
+`
+
+const Button = styled.button`
+    padding: 1rem 2rem;
+    font-weight: 600;
+    text-transform: uppercase;
+    border-radius: 0.25rem;
+    color: #1f2937;
+    background-color: #f0b322;
+    border-radius: 6px;
+    border: none;
+
+    &:hover {
+        background-color: #f0920e;
+    }
 `
 
 export default function AuthInputs() {
@@ -71,10 +87,15 @@ export default function AuthInputs() {
 		</p>
 	  </ControlContainer>
 	  <div className="actions">
-		<button type="button" className="text-button">
+		<button
+		  type="button"
+		  className="text-button"
+		>
 		  Create a new account
 		</button>
-		<button className='button' onClick={handleLogin}>Sign In</button>
+		<Button onClick={handleLogin}>
+		  Sign In
+		</Button>
 	  </div>
 	</div>
   );
